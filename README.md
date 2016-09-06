2b7aae0272872d8d05934100607e3803b510aa68
Ganesha <reekoheek@gmail.com>
2016-09-06 08:48:51 +0700

    INITIAL commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # xin-cli
+
+CLI for xin (version 2) development
+
+## commands
+
+- serve
+
+serve project to browse documentation and demo
+
---
+++ serve.js
@@ -0,0 +1,217 @@
+'use strict';
+
+const http = require('http');
+const fs = require('fs-promise');
+const path = require('path');
+const sprintf = require('sprintf-js').sprintf;
+
+var bowerJson;
+try {
+  bowerJson = require(path.resolve('bower.json'));
+} catch(e) {
+  console.error('bower.json not found');
+  return;
+}
+
+const DEBUG = process.env.XIN_CLI_DEBUG ? true : false;
+
+function apiRoot() {
+  return fs.readdir('./').then(function(files) {
+    var hasIndex = false;
+    var components = [];
+    files.forEach(function(file) {
+      if (path.extname(file) === '.html') {
+        if (file === 'index.html') {
+          hasIndex = true;
+          return;
+        }
+        var name = path.basename(file, '.html');
+        var component = {
+          name: name,
+          documentationUrl: '/_docs/' + name,
+        };
+        return components.push(component);
+      }
+    });
+
+    return {
+      name: bowerJson.name,
+      index: hasIndex,
+      components: components,
+    };
+  });
+}
+
+function serveStatic(res, file, cached) {
+  const _write = function(res, cache) {
+    res.writeHead(200, {
+      'Content-Type': cache.type,
+    });
+    res.write(cache.content);
+    res.end();
+  };
+
+  var type = 'text/html';
+  switch (path.extname(file)) {
+    case '.css':
+      type = 'text/css';
+      break;
+    case '.js':
+      type = 'application/javascript';
+      break;
+  }
+
+  if (DEBUG || !cached) {
+    return fs.readFile(file)
+      .then(function(content) {
+        _write(res, {
+          type: type,
+          content: content,
+        });
+      }, function(err) {
+        console.error('E:', err.message);
+        res.error = err;
+      });
+  }
+
+  serveStatic.caches = serveStatic.caches || {};
+  var cache = serveStatic.caches[file];
+  if (cache) {
+    _write(res, cache);
+    return Promise.resolve();
+  }
+
+  return fs.readFile(file)
+    .then(function(content) {
+      var cache = serveStatic.caches[file] = {
+        type: type,
+        content: content,
+      };
+      _write(res, cache);
+    });
+}
+
+function mwIndex(next) {
+  return function(req, res) {
+    if (req.method === 'GET') {
+      if (req.url === '/') {
+        var file = path.join(__dirname, 'templates/index.html');
+        return serveStatic(res, file);
+      } else if (req.url === '/_docs') {
+        res.writeHead(302, {
+          Location: '/_docs/'
+        });
+        res.end();
+        return Promise.resolve();
+      } else if (req.url.startsWith('/_docs/')) {
+        var file = path.join(__dirname, 'templates/docs.html');
+        return serveStatic(res, file);
+      }
+    }
+
+    return next(req, res);
+  };
+}
+
+function mwLog(next) {
+  return function(req, res) {
+    return next(req, res)
+      .then(function() {
+        if (!res.finished) {
+          res.writeHead(404);
+          res.write('Not found');
+          res.end();
+        }
+        var dt = new Date();
+        var tStr = sprintf('%02d.%03d', dt.getSeconds(), dt.getMilliseconds());
+        console.log('| %s [%s] %s %s', tStr, res.statusCode, req.method, req.url);
+      });
+  };
+}
+
+function mwBower(next) {
+  return function(req, res) {
+    if (req.method === 'GET' && req.url.startsWith('/bower_components/')) {
+      var file = path.join('.', req.url);
+      if (req.url.startsWith('/bower_components/xin-cli/')) {
+        file = path.join(__dirname, req.url.substr(25));
+      } else if (req.url.startsWith('/bower_components/' + bowerJson.name + '/')) {
+        file = req.url.substr(19 + bowerJson.name.length);
+      }
+      return serveStatic(res, file);
+    }
+
+    return next(req, res);
+  };
+}
+
+function mwApi(next) {
+  return function(req, res) {
+    if (req.url.startsWith('/xin-cli-api/')) {
+      var apiUrl = req.url.substr(12);
+
+      return Promise.resolve()
+        .then(function() {
+          switch(apiUrl) {
+            case '/':
+              return apiRoot();
+            default:
+              throw notFoundErr;
+          }
+        })
+        .then(function(body) {
+          res.writeHead(200, {
+            'Content-Type': 'application/json',
+          });
+          res.write(JSON.stringify(body || null));
+          res.end();
+        })
+        .catch(function(err) {
+          res.writeHead(err.status || 500, {
+            'Content-Type': 'application/json',
+          });
+          res.write(JSON.stringify({
+            message: err.message
+          }));
+          res.end();
+        });
+    }
+
+    return next(req, res);
+  };
+}
+
+function mwDemo(next) {
+  return function(req, res) {
+    if (req.method === 'GET' && req.url.startsWith('/demo/')) {
+      var file = path.join('demo', req.url.substr(6));
+      return serveStatic(res, file);
+    }
+    return next(req, res);
+  };
+}
+
+function mwNoop() {
+  return function(){
+    return Promise.resolve();
+  };
+}
+
+const server = http.createServer(function(req, res) {
+  mwLog(
+  mwApi(
+  mwIndex(
+  mwDemo(
+  mwBower(
+    mwNoop()
+  )))))(req, res).then(function() {
+    if (res.finished) {
+      clearTimeout(t);
+    }
+  });
+});
+
+server.listen(8080, function() {
+  console.log('XIN_CLI_DEBUG = %s', DEBUG);
+  console.log('Listening at %s...', server.address().port);
+});
